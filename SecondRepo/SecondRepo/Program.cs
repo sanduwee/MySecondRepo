c19815d602ac7a993ed199158f3ab0ee2a6c179b
sanduwee <120154440+sanduwee@users.noreply.github.com>
2024-01-25 21:44:54 -0800

    Update Program.cs
    Small change - added loop

--- SecondRepo/SecondRepo/Program.cs
+++ SecondRepo/SecondRepo/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int x = 10;
+            for(int i = 0; i < 10; i++)
+            {
+                x = x + i;
+                Console.WriteLine(x);
+            }
         }
     }
 }
